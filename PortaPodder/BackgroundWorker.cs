94ce7f57ab9775a106e81380503c259e758035fb
Brian Bourke-Martin <brianbourke75@gmail.com>
2012-09-12 10:22:03 -0500

    * Added background threads to the project * Added more meta data to the devices database object

--- PortaPodder/BackgroundWorker.cs
+++ PortaPodder/BackgroundWorker.cs
@@ -1,1 +1,139 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
 
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Text.Method;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace GPodder.PortaPodder{
+
+  /// <summary>
+  /// background task for logging in
+  /// </summary>
+  public class BackgroundWorker : AsyncTask {
+
+    #region members
+
+    /// <summary>
+    /// The stopping variable
+    /// </summary>
+    private bool stop = false;
+
+    /// <summary>
+    /// The exception which happened during
+    /// </summary>
+    private Exception problem = null;
+
+    /// <summary>
+    /// definition for the method which will be called when the worker is done
+    /// <summary> 
+    public delegate void WorkerCompleted(Exception exc);
+
+    /// <summary>
+    /// definition for the object to do the work
+    /// <summary>    
+    public delegate void WorkerDelegate(ref bool stop);
+
+    /// <summary>
+    /// The worker object
+    /// </summary>
+    private WorkerDelegate worker = null;
+
+    /// <summary>
+    /// The completed method
+    /// </summary>
+    private WorkerCompleted completed = null;
+
+    #endregion
+
+    #region construction
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GPodder.PortaPodder.BackgroundWorker"/> class.
+    /// </summary>
+    /// <param name='worker'>Worker.</param>
+    public BackgroundWorker(WorkerDelegate worker) {
+      if(worker == null) {
+        throw new ArgumentException("Worker delegate cannot be null");
+      }
+      this.worker = worker;
+    }
+
+    #endregion
+
+    #region properties
+
+    /// <summary>
+    /// Gets or sets a value indicating whether this <see cref="GPodder.PortaPodder.BackgroundWorker"/> should stop
+    /// </summary>
+    /// <value><c>true</c> if stop; otherwise, <c>false</c>.</value>
+    public bool Stop {
+      get {
+        return stop;
+      }
+      set {
+        stop = value;
+      }
+    }
+
+    /// <summary>
+    /// Occurs when worker.
+    /// </summary>
+    public WorkerDelegate Worker {
+      get {
+        return worker;
+      }
+    }
+
+    /// <summary>
+    /// Occurs when worker.
+    /// </summary>
+    public WorkerCompleted Completed {
+      set {
+        completed = value;
+      }
+      get {
+        return completed;
+      }
+    }
+
+    #endregion
+
+    #region members
+   
+    /// <summary>
+    /// Dos the in background.
+    /// </summary>
+    /// <returns>The in background.</returns>
+    /// <param name='params'>Parameters.</param>
+    protected override Java.Lang.Object DoInBackground(params Java.Lang.Object[] @params) {
+      try {
+        worker(ref stop);
+      }
+      catch(Exception exc) {
+        problem = exc;
+      }
+      return null;
+    }
+
+    /// <summary>
+    /// Raises the post execute event.
+    /// </summary>
+    /// <param name='result'>Result.</param>
+    protected override void OnPostExecute(Java.Lang.Object result) {
+      if(completed != null) {
+        completed(problem);
+      }
+    }
+
+    #endregion
+
+  }
+}
