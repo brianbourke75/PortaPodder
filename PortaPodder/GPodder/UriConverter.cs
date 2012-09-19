9ace4a1676f8b665784bb4b5f802774e18fa08c6
Brian Bourke-Martin <brianbourke75@gmail.com>
2012-09-19 10:00:42 -0400

    * Added GPL provisions for the project * Updated the launcher icon for the project

--- PortaPodder/GPodder/UriConverter.cs
+++ PortaPodder/GPodder/UriConverter.cs
@@ -1,3 +1,24 @@
+//
+//  UriConverter.cs
+//
+//  Author:
+//       Brian Bourke-Martin <brianbourke75@gmail.com>
+//
+//  Copyright (c) 2012 Brian Bourke-Martin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 using System;
 using Newtonsoft.Json;
 
@@ -15,7 +36,7 @@ namespace GPodder.DataStructures {
     /// <param name='value'>Value.</param>
     /// <param name='serializer'>Serializer.</param>
     public override void WriteJson (JsonWriter writer, object value, JsonSerializer serializer) {
-      writer.WriteValue(value.ToString());
+      writer.WriteRaw(value.ToString());
     }
 
     /// <summary>
@@ -43,4 +64,3 @@ namespace GPodder.DataStructures {
     }
   }
 }
-
